816dfd873c126eca57670ecab4fc98c2d79f2fb9
T-Seven <info@designperformance.de>
2025-10-03 13:57:59 +0200

    Update install.php

--- shoutbox/install.php
+++ shoutbox/install.php
@@ -13,15 +13,12 @@ safe_query("CREATE TABLE IF NOT EXISTS plugins_shoutbox_messages (
 ## SYSTEM #####################################################################################################################################
 
 safe_query("INSERT IGNORE INTO settings_plugins (pluginID, name, modulname, info, admin_file, activate, author, website, index_link, hiddenfiles, version, path, status_display, plugin_display, widget_display, delete_display, sidebar) VALUES
-('', 'Shoutbox', 'shoutbox', '[[lang:de]]Mit diesem Plugin könnt ihr ein shoutbox auf die Webseite anzeigen lassen.[[lang:en]]With this plugin you can display a shoutbox on the website.[[lang:it]]Con questo plugin puoi visualizzare una galleria sul sito web.', 'admin_shoutbox', 1, 'T-Seven', 'https://webspell-rm.de', 'shoutbox', '', '0.1', 'includes/plugins/shoutbox/', 1, 1, 0, 1, 'deactivated')");
+('', 'Shoutbox', 'shoutbox', '[[lang:de]]Mit diesem Plugin könnt ihr ein shoutbox auf die Webseite anzeigen lassen.[[lang:en]]With this plugin you can display a shoutbox on the website.[[lang:it]]Con questo plugin puoi visualizzare una galleria sul sito web.', 'admin_shoutbox', 1, 'T-Seven', 'https://www.nexpell.de', 'shoutbox', '', '0.1', 'includes/plugins/shoutbox/', 1, 1, 0, 1, 'deactivated')");
 
 safe_query("INSERT IGNORE INTO settings_widgets (widget_key, title, plugin, modulname) VALUES
 ('widget_shoutbox_sidebar', 'Shoutbox Sidebar', 'shoutbox', 'shoutbox')");
 
 ## NAVIGATION #####################################################################################################################################
-
-safe_query("INSERT IGNORE INTO navigation_dashboard_links (linkID, catID, name, modulname, url, sort) VALUES
-('', 9, '[[lang:de]]Shoutbox[[lang:en]]Shoutbox[[lang:it]]Shoutbox', 'shoutbox', 'admincenter.php?site=admin_shoutbox', 1)");
 
 safe_query("INSERT IGNORE INTO navigation_website_sub (snavID, mnavID, name, modulname, url, sort, indropdown) VALUES
 ('', 3, '[[lang:de]]Shoutbox[[lang:en]]Shoutbox[[lang:it]]Shoutbox', 'shoutbox', 'index.php?site=shoutbox', 1, 1)");
