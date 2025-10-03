2ac84714c015ebce290c36752f09ec2b76066428
T-Seven <info@designperformance.de>
2025-10-03 13:58:10 +0200

    Update install.php

--- sponsors/install.php
+++ sponsors/install.php
@@ -14,12 +14,12 @@ safe_query("CREATE TABLE IF NOT EXISTS plugins_sponsors (
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
 
 safe_query("INSERT IGNORE INTO plugins_sponsors (id, name, slug, logo, level, description, updated_at, userID, sort_order, is_active) VALUES
-(1, 'Firma A', 'https://firma-a.de', '1.png', 'Platin Sponsor', NULL, '2025-06-01 13:46:22', 1, 1, 1),
-(2, 'Firma B', 'https://firma-b.de', '2.png', 'Gold Sponsor', NULL, '2025-06-01 13:46:22', 1, 2, 1),
-(3, 'Firma C', 'https://firma-c.de', '3.png', 'Silber Sponsor', NULL, '2025-06-01 13:46:22', 1, 3, 1),
-(4, 'Firma D', 'https://firma-d.de', '4.png', 'Bronze Sponsor', NULL, '2025-06-01 13:46:22', 1, 4, 1),
-(5, 'Firma E', 'https://firma-e.de', '5.png', 'Partner', NULL, '2025-06-01 13:46:22', 1, 5, 1),
-(6, 'Firma F', 'https://firma-f.de', '6.png', 'Unterstützer', NULL, '2025-06-01 13:46:22', 1, 6, 1)");
+(1, 'Firma A', 'https://www.nexpell.de', '1.png', 'Platin Sponsor', NULL, '2025-06-01 13:46:22', 1, 1, 1),
+(2, 'Firma B', 'https://www.nexpell.de', '2.png', 'Gold Sponsor', NULL, '2025-06-01 13:46:22', 1, 2, 1),
+(3, 'Firma C', 'https://www.nexpell.de', '3.png', 'Silber Sponsor', NULL, '2025-06-01 13:46:22', 1, 3, 1),
+(4, 'Firma D', 'https://www.nexpell.de', '4.png', 'Bronze Sponsor', NULL, '2025-06-01 13:46:22', 1, 4, 1),
+(5, 'Firma E', 'https://www.nexpell.de', '5.png', 'Partner', NULL, '2025-06-01 13:46:22', 1, 5, 1),
+(6, 'Firma F', 'https://www.nexpell.de', '6.png', 'Unterstützer', NULL, '2025-06-01 13:46:22', 1, 6, 1)");
 
 safe_query("CREATE TABLE IF NOT EXISTS plugins_sponsors_settings (
   sponsorssetID INT(11) NOT NULL AUTO_INCREMENT,
@@ -32,7 +32,7 @@ safe_query("INSERT IGNORE INTO plugins_sponsors_settings (sponsorssetID, sponsor
 ## SYSTEM #####################################################################################################################################
 
 safe_query("INSERT IGNORE INTO settings_plugins (pluginID, name, modulname, info, admin_file, activate, author, website, index_link, hiddenfiles, version, path, status_display, plugin_display, widget_display, delete_display, sidebar) VALUES
-('', 'Sponsors', 'sponsors', '[[lang:de]]Mit diesem Plugin könnt ihr eure Sponsoren anzeigen lassen.[[lang:en]]With this plugin you can display your sponsors.[[lang:it]]Con questo plugin puoi visualizzare i tuoi sponsor.', 'admin_sponsors', 1, 'T-Seven', 'https://webspell-rm.de', 'sponsors', '', '0.2', 'includes/plugins/sponsors/', 1, 1, 1, 1, 'deactivated')");
+('', 'Sponsors', 'sponsors', '[[lang:de]]Mit diesem Plugin könnt ihr eure Sponsoren anzeigen lassen.[[lang:en]]With this plugin you can display your sponsors.[[lang:it]]Con questo plugin puoi visualizzare i tuoi sponsor.', 'admin_sponsors', 1, 'T-Seven', 'https://www.nexpell.de', 'sponsors', '', '0.2', 'includes/plugins/sponsors/', 1, 1, 1, 1, 'deactivated')");
 
 ## NAVIGATION #####################################################################################################################################
 
